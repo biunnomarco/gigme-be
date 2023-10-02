48076e1771e242757ceb03ff34a3bd4f4dc052e1
Marco <biunnomarco@gmail.com>
2023-10-02 20:25:12 +0200

    cloudinary

--- middlewares/uploadProPic.js
+++ middlewares/uploadProPic.js
@@ -1,10 +1,10 @@
-import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import multer from 'multer';
+const cloudinary = require ('cloudinary').v2;
+const {CloudinaryStorage} = require('multer-storage-cloudinary')
+const multer = require ('multer')
 
 
 cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET, 
   });
@@ -23,4 +23,4 @@ const proPic = multer({
   storage: storageProPic
 })
 
-export default proPic
+module.exports = proPic
