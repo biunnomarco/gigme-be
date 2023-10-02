56c06b7b3d6576db409262adf2ffe31ca14dec04
Marco <biunnomarco@gmail.com>
2023-10-02 20:22:20 +0200

    cloudinary

--- middlewares/uploadProPic.js
+++ middlewares/uploadProPic.js
@@ -4,7 +4,7 @@ import multer from 'multer';
 
 
 cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET, 
   });
